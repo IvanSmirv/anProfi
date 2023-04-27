48b3095ff264f8202e28ae2afd6c7bf382af2379
IvanSmirv <96989046+IvanSmirv@users.noreply.github.com>
2023-04-27 13:16:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -52,4 +52,5 @@ document
   .addEventListener("click", function () {
     this.classList.toggle("active");
     document.querySelector(".header__content").classList.toggle("active");
-  });
+    document.body.classList.toggle("block");
+});
